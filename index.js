729f537713ef8273d49814a3668d8165659e15d5
sagun basnet <basnetsagun8@gmail.com>
2025-01-29 14:56:06 +0545

    dom started

---
+++ index.js
@@ -0,0 +1,43 @@
+// const a = true;
+// const b = "Himal";
+// const a = add(3, 2);
+// console.log(add('himal', 'asdf'));
+// ;
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// var a = [1, 2, 3, 4, "sagar", false, add(2,3), {a: true}];
+// console.log(a);
+
+// var obj = {
+//     name: "sagar",
+//     age: 23,
+//     phone: '98123212',
+//     myFun: add(),
+//     boo: true,
+//     arr : [1, 2, 3, 4],
+//     myObj: {name: "sagar", age:32}
+// }
+
+
+// var data = [
+//     {
+//         id: 1,
+//         name: "sagar",
+//         age: 23,
+//         address: "asdfasdfsd"
+//     },
+//     {
+//         id: 2,
+//         name: "sagar",
+//         age: 23,
+//         address: "asdfasdfsd"
+//     },
+// ]
+// console.log(data[0]);
+
+
+
+
