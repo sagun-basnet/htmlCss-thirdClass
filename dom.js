9a486523e9e1e17e4b61b83f884047f97c3b1ffc
sagun basnet <basnetsagun8@gmail.com>
2025-02-05 14:38:37 +0545

    db basic operation

--- dom.js
+++ dom.js
@@ -1,10 +1,18 @@
-const myHeading = document.getElementsByClassName("heading");
-const head = document.getElementById("head");
-const hh = document.getElementsByTagName("h1");
+// const myHeading = document.getElementsByClassName("heading");
+// const head = document.getElementById("head");
+// const hh = document.getElementsByTagName("h1");
 
-const head1 = document.querySelectorAll("h1");
+// const head1 = document.querySelectorAll("h1");
 
-console.log("Class Name: ", myHeading);
-console.log("By Id: ", head);
-console.log("By tag name: ", hh);
-console.log("By query selector: ", head1);
+// console.log("Class Name: ", myHeading);
+// console.log("By Id: ", head);
+// console.log("By tag name: ", hh);
+// console.log("By query selector: ", head1);
+
+const btn = document.querySelector(".btnClick");
+const div = document.querySelector(".myDiv");
+
+btn.addEventListener("click", () => {
+  div.style.backgroundColor = "blue";
+  div.style.margin = "2rem";
+});
